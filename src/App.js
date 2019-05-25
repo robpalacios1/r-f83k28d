b5a868d565aa4c6306b69ea03a313e209bf31a76
Roberto <robpalacios1@hotmail.com>
2019-05-25 00:37:40 -0500

    increase number

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+	  super();
+	  this.state = {
+	  	count: 0
+	 }
+} 
+
+	
   render() {
     return (
       <div>
         <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <button id="inc" onClick={this.addValue.bind(this)}>Incrementa</button>
       </div>
     );
   }
+
+	addValue(){
+		this.setState({
+			count: this.state.count + 1
+		});
+	}
 }
 
 export default App;
